5ade2ee09924751647bade09ef53ed1a893e9f80
Nennneko5787 <nennneko@tensi.org>
2022-12-17 12:08:32 +0900

    meow*5

--- source/APIStuff.hx
+++ source/APIStuff.hx
@@ -4,4 +4,9 @@ class APIStuff
 {
 	public static var API:String = "";
 	public static var EncKey:String = "";
+	public static var gameID:String = "";
+	public static var comWeek:String = "";
+	public static var fcs:String = "";
+	public static var comSong:String = "";
+	public static var boards:String = "";
 }
